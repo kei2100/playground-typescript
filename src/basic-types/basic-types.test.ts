8a4a6dbf1ebdc782d51a2a3975d94eba7a1f7437
kei2100 <kei.arima.2100@gmail.com>
2018-06-05 11:19:45 +0900

    add tuple

--- src/basic-types/basic-types.test.ts
+++ src/basic-types/basic-types.test.ts
@@ -4,3 +4,11 @@ test('define array', () => {
 
   expect(l1).toEqual(l2);
 });
+
+test('define tuple', () => {
+  let tuple: [string, number];
+  tuple = ['foo', 1];
+
+  expect(tuple[0]).toBe('foo');
+  expect(tuple[1]).toBe(1);
+});
